d612ff24799066da08350e8a98892c1712d76773
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2025-06-04 17:51:33 +0900

    Update servo_slow.py

--- servo_slow.py
+++ servo_slow.py
@@ -4,14 +4,14 @@ import time
 pwm = Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(50)
 
-pwm.set_pwm(0,0,100)
+pwm.set_pwm(0,0,110)
 time.sleep(1)
 
 while True:
-  for duty in range(100, 561):
+  for duty in range(110, 551):     # duty : 100~560 
     pwm.set_pwm(0,0,duty)
     time.sleep(0.05)
-  for duty in range(560, 99, -1):
+  for duty in range(550, 109, -1):
     pwm.set_pwm(0,0,duty)
     time.sleep(0.05)
 
